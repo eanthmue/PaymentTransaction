241f5adad33561d7798d0eadca08194e379489c4
Eant Hmue Htan <e.h.htan@gmail.com>
2020-11-14 22:38:08 +0630

    Added Infrastructure project

---
+++ Common.Infrastructure/Repositories/IPaymentUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Entities;
+
+namespace Common.Infrastructure.Repositories
+{
+    public interface IPaymentUnitOfWork
+    {
+        ITransactionRepository<Transaction> TransactionRepository { get; }
+        void SaveChangesAsync();
+        void Dispose();
+    }
+}
---
+++ Common.Infrastructure/Repositories/ITransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Infrastructure.Repositories
+{
+    public interface ITransactionRepository<TTransaction> where TTransaction : Transaction
+    {
+        Task AddRange(IList<TTransaction> transactions);
+        Task<IList<TTransaction>> GetByCurrency(string currencyCode);
+        Task<IList<TTransaction>> GetByDateRange(DateTime fromDate, DateTime toDate);
+        Task<IList<TTransaction>> GetByStatus(string status);
+    }
+}
