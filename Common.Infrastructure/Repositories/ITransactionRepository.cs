0eaafd08ad2a7eb90179c75bab3050f0e80ae136
Eant Hmue Htan <e.h.htan@gmail.com>
2020-11-16 02:43:45 +0630

    Added Service Interface

--- Common.Infrastructure/Repositories/ITransactionRepository.cs
+++ Common.Infrastructure/Repositories/ITransactionRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Common.Entities;
 
 namespace Common.Infrastructure.Repositories
 {
     public interface ITransactionRepository<TTransaction> where TTransaction : Transaction
     {
-        Task AddRange(IList<TTransaction> transactions);
+        Task BulkInsert(IList<TTransaction> transactions);
         Task<IList<TTransaction>> GetByCurrency(string currencyCode);
         Task<IList<TTransaction>> GetByDateRange(DateTime fromDate, DateTime toDate);
         Task<IList<TTransaction>> GetByStatus(string status);
---
+++ Common.Infrastructure/Services/ITransactionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Entities;
+
+namespace Common.Infrastructure.Services
+{
+    public interface ITransactionService
+    {
+        Task BulkInsert(IList<Transaction> transactions);
+        Task<IList<Transaction>> GetByCurrency(string currencyCode);
+        Task<IList<Transaction>> GetByDateRange(DateTime fromDate, DateTime toDate);
+        Task<IList<Transaction>> GetByStatus(string status);
+    }
+}
